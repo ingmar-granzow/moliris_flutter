4c5708309be5cc67c5502d14333f274a603fe051
Ingmar Rötzler <mail@roetzler.de>
2020-04-11 16:22:26 +0200

    add stateful Event widget that holds the event items

--- lib/main.dart
+++ lib/main.dart
@@ -3,30 +3,88 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MaterialApp(
     title: 'Moliris Event Planner',
-    home: TutorialHome(),
+    home: Event(),
   ));
 }
 
-class ItemWidget extends StatefulWidget {
+class Event extends StatefulWidget {
   @override
-  _ItemWidgetState createState() => _ItemWidgetState();
+  _EventState createState() => _EventState();
+}
+
+class _EventState extends State<Event> {
+  String _name = 'Himmelfahrtskommando 2020';
+  // description
+  // identifier
+  var _items = [
+    {'name': 'Grill', 'notes': 'Kleiner Holzkohlegrill', 'person': null, 'date': DateTime.now()},
+  ];
+
+  void _handleAddItem() {
+    setState(() {
+      _items.add({'name': 'Holzkohle', 'notes': '1 Sack', 'person': null, 'date': DateTime.now()});
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    List<Widget> renderItems = [];
+    _items.forEach((item) {
+        renderItems.add(ItemWidget(name: item['name'], notes: item['notes']));
+      });
+
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          icon: Icon(Icons.menu),
+          tooltip: 'Navigation menu',
+          onPressed: null,
+        ),
+        title: Text(_name),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.search),
+            tooltip: 'Search',
+            onPressed: null,
+          ),
+        ],
+      ),
+      body: ListView(
+        children: renderItems,
+      ),
+      floatingActionButton: FloatingActionButton(
+        tooltip: 'Add',
+        child: Icon(Icons.add),
+        onPressed: _handleAddItem,
+      ),
+    );
+  }
+}
+
+class ItemWidget extends StatefulWidget {
+  ItemWidget({Key key, this.name: '', this.notes: ''})
+      : super(key: key);
+
+  final String name;
+  final String notes;
+
+   _ItemWidgetState createState() => _ItemWidgetState();
 }
 
 class _ItemWidgetState extends State<ItemWidget> {
-  String _name = 'name';
-  String _notes = 'note';
   String _person;
 
   @override
   Widget build(BuildContext context) {
     return Card(
       child: ListTile(
-        title: Text(_name),
+        title: Text(widget.name),
         subtitle: Text(
-          _notes
+          widget.notes,
+          overflow: TextOverflow.ellipsis,
         ),
         trailing: (_person != null ? Text(_person) : IconButton(
-          icon: Icon(Icons.add),
+          icon: Icon(Icons.edit),
           onPressed: _addPerson,
         )),
       ),
@@ -37,70 +95,5 @@ class _ItemWidgetState extends State<ItemWidget> {
     setState(() {
       _person = 'Ingmar';
     });
-  }
-}
-
-class TutorialHome extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    // Scaffold is a layout for the major Material Components.
-    return Scaffold(
-      appBar: AppBar(
-        leading: IconButton(
-          icon: Icon(Icons.menu),
-          tooltip: 'Navigation menu',
-          onPressed: null,
-        ),
-        title: Text('Himmelfahrtskommando 2020'),
-        actions: <Widget>[
-          IconButton(
-            icon: Icon(Icons.search),
-            tooltip: 'Search',
-            onPressed: null,
-          ),
-        ],
-      ),
-      // body is the majority of the screen.
-      body: ListView(
-        children: <Widget>[
-          Card(
-            child: ListTile(
-              title: Text('Grill'),
-              subtitle: Text(
-                'Kleiner Holzkohlegrill'
-              ),
-              trailing: Text(
-                'Ingmar'
-              ),
-            ),
-          ),
-          Card(
-            child: ListTile(
-              title: Text('Holzkohle'),
-              subtitle: Text(
-                '1 Sack'
-              ),
-              trailing: Icon(Icons.add),
-            ),
-          ),
-          Card(
-            child: ListTile(
-              title: Text('Grillzange'),
-              subtitle: Text(
-                'Eine ganz tolle, feuerfeste Grillzange mit einer langen Beschreibung',
-                overflow: TextOverflow.ellipsis,
-              ),
-              trailing: Icon(Icons.add),
-            ),
-          ),
-          ItemWidget(),
-        ],
-      ),
-      floatingActionButton: FloatingActionButton(
-        tooltip: 'Add', // used by assistive technologies
-        child: Icon(Icons.add),
-        onPressed: null,
-      ),
-    );
   }
 }
